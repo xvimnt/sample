afeb2a3f84387bf8e01999846e32a83b11466567
Xavi <jvmonteros98@gmail.com>
2022-10-06 20:07:38 -0500

    sample landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Landing from "./screens/Landing";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to my first serverless app</h1>
-      <p>
-        This is gonna be used as a template for all others apps.
-      </p>
+      <Landing />
     </div>
   );
 }
