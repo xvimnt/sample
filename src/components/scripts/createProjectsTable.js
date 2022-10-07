8c6330c9be8bedbd9164524d899bb635ff34b656
Xavi <jvmonteros98@gmail.com>
2022-10-07 12:45:02 -0500

    scripts to create tables

---
+++ src/components/scripts/createProjectsTable.js
@@ -0,0 +1,45 @@
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: "us-east-1"
+});
+
+var dynamodb = new AWS.DynamoDB();
+
+var params = {
+  TableName: "Projects",
+  KeySchema: [
+    // Partition Key
+    { AttributeName: "title", KeyType: "HASH" },
+    // Sort Keys
+    { AttributeName: "action", KeyType: "RANGE"}
+  ],
+  AttributeDefinitions: [
+    { AttributeName: "title", AttributeType: "S" },
+    { AttributeName: "action", AttributeType: "S" },
+    { AttributeName: "text", AttributeType: "S" }
+  ],
+  LocalSecondaryIndexes: [
+    {
+      IndexName: "TextIndex",
+      KeySchema: [
+        { AttributeName: "title", KeyType: "HASH" },
+        { AttributeName: "text", KeyType: "RANGE" }
+      ],
+      Projection: {
+        ProjectionType: "KEYS_ONLY"
+      }
+    }
+  ], 
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 10,
+    WriteCapacityUnits: 10
+  }
+};
+
+dynamodb.createTable(params, function(err, data) {
+  if (err)
+    console.error("Unable to create table: ", JSON.stringify(err, null, 2))
+  else
+    console.log("Created table with description: ", JSON.stringify(data, null, 2))
+});
