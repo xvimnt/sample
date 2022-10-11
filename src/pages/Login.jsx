ad63344770a26d83e41c6daddb810fb2abd1c196
Xavi <jvmonteros98@gmail.com>
2022-10-10 19:13:24 -0500

    added icons

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,9 +3,12 @@ import { Wrapper } from "../style/components"
 // Sections
 import Footer from "../components/Sections/Footer"
 import TopMainNavbar from "../components/Nav/TopMainNavbar";
+// Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 
 export default function Login() {
-  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [isLoading, setIsLoading] = useState(false)
 
@@ -13,50 +16,49 @@ export default function Login() {
     <>
       <TopMainNavbar />
       <Wrapper>
-        <form>
-          <div class="form-outline mb-4">
-            <input type="email" id="form2Example1" class="form-control" />
-            <label class="form-label" for="form2Example1">Email address</label>
-          </div>
+        <section class="vh-100 gradient-custom">
+          <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+              <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div class="card bg-dark text-white" >
+                  <div class="card-body p-5 text-center">
 
-          <div class="form-outline mb-4">
-            <input type="password" id="form2Example2" class="form-control" />
-            <label class="form-label" for="form2Example2">Password</label>
-          </div>
+                    <div class="mb-md-5 mt-md-4 pb-5">
 
-          <div class="row mb-4">
-            <div class="col d-flex justify-content-center">
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-                <label class="form-check-label" for="form2Example31"> Remember me </label>
+                      <h2 class="fw-bold mb-2">Login</h2>
+                      <p class="text-white-50 mb-5">Please enter your login and password!</p>
+
+                      <div class="form-outline form-white mb-4">
+                        <input type="email" id="typeEmailX" class="form-control form-control-lg" value={email} onChange={(e) => {setEmail(e.target.value)}}/>
+                        <label class="form-label" for="typeEmailX">Email</label>
+                      </div>
+
+                      <div class="form-outline form-white mb-4">
+                        <input type="password" id="typePasswordX" class="form-control form-control-lg" value={password} onChange={(e) => {setPassword(e.target.value)}}/>
+                        <label class="form-label" for="typePasswordX">Password</label>
+                      </div>
+
+                      <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+
+                      <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+
+                      <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                        <a href="#!" class="text-white"><FontAwesomeIcon icon={faGoogle} /></a>
+                      </div>
+
+                    </div>
+
+                    <div>
+                      <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
+                      </p>
+                    </div>
+
+                  </div>
+                </div>
               </div>
             </div>
-
-            <div class="col">
-              <a href="#!">Forgot password?</a>
-            </div>
           </div>
-
-          <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
-
-          <div class="text-center">
-            <p>Not a member? <a href="#!">Register</a></p>
-            <p>or sign up with:</p>
-
-            <button type="button" class="btn btn-link btn-floating mx-1">
-              <i class="fab fa-google"></i>
-            </button>
-
-            <button type="button" class="btn btn-link btn-floating mx-1">
-              <i class="fab fa-twitter"></i>
-            </button>
-
-            <button type="button" class="btn btn-link btn-floating mx-1">
-              <i class="fab fa-github"></i>
-            </button>
-          </div>
-        </form>
-
+        </section>
       </Wrapper>
       <Footer />
     </>
